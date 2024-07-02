8b90c840c15024209703d2a675b7e2fdc08c977b
Qin Aoxiang <130543179+JoshAQin@users.noreply.github.com>
2024-07-02 11:21:14 +0800

    Update deploy.py

--- template/deploy.py
+++ template/deploy.py
@@ -2,22 +2,21 @@ from pynq import allocate
 from pynq import Overlay
 import numpy as np
 import time
-import timeit
 import sys
 from utils import read_graph
 
 alg_name = sys.argv[1]
-# exec("from {0}.{0} import {0}".format(alg_name))
 
 if alg_name in ["homo2","heter","msssp"]:
     weighted = True
 else:
     weighted = False
 
+dataset = sys.argv[2]
 if not weighted:
-    dataset_name = "../datasets/testGraph"
+    dataset_name = "./datasets/{}.txt".format(dataset)
 else:
-    dataset_name = "../datasets/testGraph_w"
+    dataset_name = "./datasets/{}_w.txt".format(dataset)
 
 num_jobs = 8
 g = read_graph(dataset_name)
@@ -25,26 +24,20 @@ num_nodes = g.num_nodes
 num_edges = g.num_edges
 print("# nodes: {}, # edges: {}".format(g.num_nodes, g.num_edges))
 
+buffer_size = 2048
 if not weighted:
-    inputs = allocate(shape=(num_nodes+num_edges,),dtype=np.int32)
-    np_inputs = np.concatenate((g.degrees, g.edges))
+    inputs = allocate(shape=(buffer_size,),dtype=np.int32)
+    np_inputs = np.concatenate((g.degrees, g.edges)).astype(np.int32)
 else:
-    inputs = allocate(shape=(num_nodes+num_edges+num_edges,),dtype=np.int32)
-    np_inputs = np.concatenate((g.degrees, g.edges, g.weights))
+    inputs = allocate(shape=(buffer_size,),dtype=np.int32)
+    np_inputs = np.concatenate((g.degrees, g.edges, g.weights)).astype(np.int32)
 
-# if alg_name == "pagerank":
-#     datatype = np.float32
-# else:
-#     datatype = np.int32
 datatype = np.int32
 
 res = allocate(shape=(num_nodes,),dtype=datatype)
 np_res = np.zeros((num_nodes,), dtype=datatype)
-# correct_res = np.loadtxt(dataset_name+"_res.txt",dtype=np.int32)
 
-np.copyto(inputs,np_inputs)
-
-overlay = Overlay("{0}/{0}.bit".format(alg_name))
+overlay = Overlay("./{0}/{0}.bit".format(alg_name))
 dma = overlay.axi_dma_0
 print("Bitstream loaded")
 exec("ip = overlay.{}_core".format(alg_name))
@@ -54,37 +47,37 @@ def xcel_func(start_node=0):
     ip.write(0x18, num_edges)
     ip.write(0x20, num_jobs)
     ip.write(0x28, start_node)
-    dma.sendchannel.transfer(inputs)
-    dma.recvchannel.transfer(res)
-    ip.write(0x00, 0x1)
+    ip.write(0x00, 0x01)
+    # transfer inputs
+    for i in range(len(np_inputs) // buffer_size):
+        inputs[:] = np_inputs[i*buffer_size : (i+1)*buffer_size]
+        dma.sendchannel.transfer(inputs)
+        dma.sendchannel.wait()
+    start_idx = len(np_inputs) // buffer_size * buffer_size
+    size = len(np_inputs) % buffer_size
+    new_inputs = allocate((size,),dtype=np.int32,cacheable=1)
+    new_inputs[:] = np_inputs[start_idx:start_idx+size]
+    dma.sendchannel.transfer(new_inputs)
+    dma.sendchannel.wait()
+    # transfer outputs
+    for i in range(num_nodes // buffer_size):
+        dma.recvchannel.transfer(res)
+        dma.recvchannel.wait()
+        np_res[i*buffer_size:(i+1)*buffer_size] = res[:]
+    start_idx = num_nodes // buffer_size * buffer_size
+    size = num_nodes % buffer_size
+    new_res = allocate((size,),dtype=np.int32)
+    dma.recvchannel.transfer(new_res)
+    dma.recvchannel.wait()
+    np_res[start_idx:start_idx+size] = new_res[:]
     isready = ip.read(0x00)
     while (isready == 1):
         isready = ip.read(0x00)
-    dma.sendchannel.wait()
-    dma.recvchannel.wait()
-    return res
+    return np_res
 
 print("Begin execution...")
 start_time = time.time()
-res_xcel = xcel_func(0)
+res_xcel = xcel_func(2)
+print(res_xcel)
 exec_time = time.time() - start_time
 print("FPGA Time: {:.4f}s".format(exec_time))
-np.copyto(np_res, res_xcel)
-# print(np.array_equal(np_res, correct_res))
-
-# exec("cpu_func = {}()".format(alg_name))
-# start_time = time.time()
-# res_cpu = cpu_func(g)
-# exec_time = time.time() - start_time
-# print("CPU Time: {:.4f}s".format(exec_time))
-# print(np.array_equal(res_cpu, correct_res))
-
-print(res_xcel)
-# print(res_cpu)
-# print(np.array_equal(np_res,res_cpu))
-
-fpga_time = timeit.timeit(stmt="xcel_func()", number=1, globals=globals())
-# cpu_time = timeit.timeit(stmt="cpu_func(g)", number=10, globals=globals())
-print("FPGA Mean Time: {:.4f}s".format(fpga_time))
-# print("CPU Mean Time: {:.4f}s".format(cpu_time))
-# print("Speedup {:.2f}x".format(cpu_time / fpga_time))
